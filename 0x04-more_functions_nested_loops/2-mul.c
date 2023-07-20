5c5eace0efbcd03c4470b2bdb48190ff0a097976
ziliwalid <walidzili19@gmail.com>
2023-07-20 12:09:56 -0700

    second commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplication of two ints 
+* @a: first int
+* @b: second int
+* Return: product of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
