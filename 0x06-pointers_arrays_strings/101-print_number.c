3452e61b4714708a798da9733f92ac5cd9d6e75d
ziliwalid <walidzili19@gmail.com>
2023-07-27 07:02:49 -0700

    crazy taks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_number - putchar magic
+  * @n: Number to print
+  * Return: void nada
+  */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	x = n;
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar(x % 10 + '0');
+}
