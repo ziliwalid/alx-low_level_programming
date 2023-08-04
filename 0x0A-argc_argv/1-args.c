34c2d13497dc58e3a83d151d9e08e3511451f005
ziliwalid <walidzili19@gmail.com>
2023-08-04 04:18:18 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
