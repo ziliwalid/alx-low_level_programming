1517d2307f864c9834e5ad082dbc6975fbcd4c85
ziliwalid <walidzili19@gmail.com>
2023-08-17 03:37:14 -0700

    maybe needs some fixes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,36 +1,50 @@
 #include "variadic_functions.h"
+
 /**
- * print_all - Prints anything
- * @format: args
- * Return: void
+ * print_all - prints args
+ * @format: format param
+ * Return: void, NADAAAAAAAAAA
  */
+
 void print_all(const char * const format, ...)
 {
-	int i;
 	char *str;
+	int status;
 	va_list vaLI;
+	int i;
 
 	va_start(vaLI, format);
-	if (format != NULL)
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
-		while (format[i] != '\0')
+		switch (format[i])
 		{
-			str = va_arg(vaLI, char*);
-
-			if (str == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
+			case 'c':
+				printf("%c", va_arg(vaLI, int));
+				status = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(vaLI, int));
+				status = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(vaLI, double));
+				status = 0;
+				break;
+			case 's':
+				str = va_arg(vaLI, char*);
+				if (str == NULL)
+					str = "(nil)";
 				printf("%s", str);
-			}
-
-			if ((i != n - 1) && (separator != NULL))
-			{
-				printf("%s", separator);
-			}
+				status = 0;
+				break;
+			default:
+				status = 1;
+				break;
 		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
 	}
 	printf("\n");
 	va_end(vaLI);
