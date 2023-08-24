dc6e4b80926fc9e011cdfa84459ab6fef4c5f2cf
ziliwalid <walidzili19@gmail.com>
2023-08-24 05:35:53 -0700

    mandatory done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,6 +5,7 @@
  * @c : char pram
  * Return: length
  */
+
 int lengthSTR(const char *c)
 {
 	int i = 0;
@@ -15,39 +16,37 @@ int lengthSTR(const char *c)
 	}
 	return (i);
 }
-
 /**
  * add_node_end - adds a new node in the end
  * @head: param
  * @str: str param
  * Return: pointer to the new node
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newN, *tmpN;
+	list_t *new, *tmp;
 
 	if (str == NULL)
 		return (NULL);
-	newN = malloc(sizeof(list_t));
-	if (newN == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	newN->str = strdup(str);
-	if (newN->str == NULL)
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
-		free(newN);
+		free(new);
 		return (NULL);
 	}
-	newN->len = lengthSTR(newN->str);
-	newN->next = NULL;
+	new->len = _lengthSTR(new->str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = newN;
-		return (newN);
+		*head = new;
+		return (new);
 	}
-	tmpN = *head;
-	while (tmpN->next)
-		tmpN = tmpN->next;
-	tmpN->next = newN;
-	return (newN);
-
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
