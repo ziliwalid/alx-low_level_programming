dce6451ed512940f1d2670821559eaad336a0b72
ziliwalid <walidzili19@gmail.com>
2023-08-24 05:25:37 -0700

    mandatory done

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * lengthSTR - gets the length of a string
+ * @c : char pram
+ * Return: length
+ */
+int lengthSTR(const char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node in the end
+ * @head: param
+ * @str: str param
+ * Return: pointer to the new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newN, *tmpN;
+
+	if (str == NULL)
+		return (NULL);
+	newN = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	newN->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(newN);
+		return (NULL);
+	}
+	newN->len = lengthSTR(new->str);
+	newN->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newN;
+		return (newN);
+	}
+	tmpN = *head;
+	while (tmp->next)
+		tmpN = tmpN->next;
+	tmpN->next = newN;
+	return (new);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - frees the prisoner
+ * @head: param
+ * Return: void nada
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
