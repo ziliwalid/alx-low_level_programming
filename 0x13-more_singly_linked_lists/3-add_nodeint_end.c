077efd7f07119652276e4723759dfeaa3f1d2c15
ziliwalid <walidzili19@gmail.com>
2023-08-29 03:28:40 -0700

    second commit and fixes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,13 +23,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-	*head = new;
+		*head = new;
 		return (new);
 	}
 	tmp = *head;
 	while (tmp->next != NULL)
 	{
-		temp = tmp->next;
+		tmp = tmp->next;
 	}
 	tmp->next = new;
 	return (new);
