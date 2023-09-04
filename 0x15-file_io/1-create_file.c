b479cc1d76b176812a209ea4c1ff598fc144f508
ziliwalid <walidzili19@gmail.com>
2023-09-04 04:20:39 -0700

    second commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-        	return (-1);
+		return (-1);
 	}
 	if (text_content != NULL)
 	{
-        	while (text_content[len])
-        		len++;
+		while (text_content[len])
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
