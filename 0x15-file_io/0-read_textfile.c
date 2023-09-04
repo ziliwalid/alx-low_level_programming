4a82e837ca8e9d2c038ca1e81c0267f835883117
ziliwalid <walidzili19@gmail.com>
2023-09-04 03:15:29 -0700

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * read_textfile - Reads a text file/prints it
+ * @filename: file name param
+ * @letters: letters param
+ * Return: 0 if it fails or (o/w)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o; /*for open*/
+	ssize_t r; /*for read*/
+	ssize_t w; /*for write*/
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(o);
+	return (w);
+}
